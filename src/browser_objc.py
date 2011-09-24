104b36f8a80cd9edf7c5cd57633d183efe1c2377
Nat Duca <natduca@gmail.com>
2011-09-24 12:30:53 -0700

    First pass implementation of objc-based browser.

--- src/browser_objc.py
+++ src/browser_objc.py
@@ -17,7 +17,61 @@ import message_loop
 import sys
 from Foundation import *
 from AppKit import *
+from WebKit import *
 
+class BrowserObjc(NSWindow,browser.BrowserBase):
+  def init(self):
+    size = NSMakeRect(0,0,800,600)
+    NSWindow.initWithContentRect_styleMask_backing_defer_(
+      self,
+      size,
+      NSTitledWindowMask | NSClosableWindowMask | NSResizableWindowMask | NSMiniaturizableWindowMask,
+      NSBackingStoreBuffered,
+      False)
+    self.setTitle_("Hello world")
+    self.contentView().setAutoresizesSubviews_(True)
 
-"""Alias for BrowserGtk"""
-Browser = None
+    self._webview = WebView.new()
+    self._webview.setFrame_(size)
+    self._webview.setAutoresizingMask_(NSViewWidthSizable | NSViewHeightSizable)
+    self._webview.setFrameLoadDelegate_(self)
+    self.contentView().addSubview_(self._webview)
+
+    self._loaded = False
+    return self
+  
+  def load_url(self, url):
+    self._loaded = False
+    self._webview.setMainFrameURL_(url)
+
+  # frame load delegate methods
+  def didFinishLoadForFrame_(self, frame):
+    print "didfinishloading"
+    if frame == self._webview.mainFrame():
+      logging.debug("Load finPished")
+      self._loaded = True
+
+  def run_javascript(self, script, require_loaded = True):
+    if require_loaded and self._loaded == False:
+      return None
+    # this wraps the script in an eval, then a try-catch, and then tostrings the result in a null/undef-safe way
+    # when you dont do this, it takes down WxPython completely.
+    quoted_script = script.replace('"', '\\"')
+    # run the script inside an eval, inside a catch, inside a function.
+    cmd = """(function() { var t = (((function() { try { return eval("%s;"); } catch(ex) { return ex; } })())); if (t === null) return 'null'; if (t === undefined) return 'undefined'; return t.toString(); })();""" % quoted_script
+    return self._webview.stringByEvaluatingJavaScriptFromString_(cmd)
+
+  def show(self):
+    self.makeKeyAndOrderFront_(self)
+  
+  def windowWillClose_(self, notification):
+    print "foo"
+    app.terminate_(self)
+
+  def quit_(self, a):
+    print "quit"
+
+"""Alias for BrowserObjc"""
+def Browser():
+  b = BrowserObjc.new()
+  return b
