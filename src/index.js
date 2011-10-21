ff7bd17fac5efd1b80d6b36bcbceba9f71b4f154
Nat Duca <natduca@gmail.com>
2011-10-21 01:49:00 -0700

    Make timeline view a global for debugging.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 // Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
+var g_timelineView;
 (function() {
   var include = function(path) {
     document.write('<script src="' + path + '"></script>');
@@ -17,6 +18,7 @@
 
   function onDOMContentLoaded() {
     timelineView = $('timeline-view');
+    g_timelineView = timelineView;
     cr.ui.decorate(timelineView, tracing.TimelineView);
     chrome.send('ready');
   }
